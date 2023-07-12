12cce945bca8343d1aad878cabb3b951e5ee034b
JohnsonAudreyRalph <kalilinuxhackmaster@gmail.com>
2023-07-12 18:12:49 +0700

    Adding Chatter, Compute Fields, Sequential Value, Search View

--- Project/HR_Management_Solutions/Human_Resource_Management/__manifest__.py
+++ Project/HR_Management_Solutions/Human_Resource_Management/__manifest__.py
@@ -14,6 +14,7 @@
     'demo': [],
     'data': [
         'security/ir.model.access.csv',
+        'data/sequences.xml',
         'views/Employee_Information.xml',
     ],
     'license': 'LGPL-3',
--- Project/HR_Management_Solutions/Human_Resource_Management/models/Employee_Information.py
+++ Project/HR_Management_Solutions/Human_Resource_Management/models/Employee_Information.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
 
@@ -21,9 +21,31 @@ class EmployeeInformation(models.Model):
     education_level = fields.Char(string='Trình độ học vấn', tracking=True)
     marital_status = fields.Boolean(string='Tình trạng hôn nhân', default=False, tracking=True)
 
+    config_name = fields.Char(string='Họ tên', compute='_compute_config_name', store=True)
+    ref = fields.Char(string='Mã nhân viên', default=lambda self: _('Tạo thông tin mới'))
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            vals['ref'] = self.env['ir.sequence'].next_by_code('employee.information')
+        return super(EmployeeInformation, self).create(vals_list)
+
+    @api.depends('name')
+    def _compute_config_name(self):
+        for record in self:
+            if record.name:
+                # Viết hoa chữ cái đầu tiên và các chữ đầu sau dấu cách
+                config_name_parts = record.name.split()
+                config_name_parts = [part.capitalize() for part in config_name_parts]
+                config_name = " ".join(config_name_parts)
+                # Loại bỏ khoảng trắng thừa ở đầu, giữa và cuối chuỗi
+                processed_string = config_name.strip()
+                record.config_name = processed_string
+            else:
+                record.config_name = ''
+
     @api.constrains('email')
     def _validate_email(self):
         for record in self:
             if record.email and '@' not in record.email:
                 raise ValidationError("Địa chỉ email không hợp lệ!")
-
